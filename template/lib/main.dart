a8a1763f99188c0dae7bf8c88429284ee8e7aa9b
Mantresh Khurana <mantreshkhurana@gmail.com>
2022-07-20 05:05:03 +0530

    New schema added

--- template/lib/main.dart
+++ template/lib/main.dart
@@ -1,7 +1,10 @@
 // don't change the source code, this is a very important file
 
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:flutter_webview_pro/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'screens/screens.dart';
 
 void main() {
@@ -32,31 +35,77 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
-  initState() {
+  void initState() {
     super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+    if (Platform.isIOS) WebView.platform = CupertinoWebView();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: WebviewScaffold(
-          url: 'https://WEBSITE',
-          withZoom: false,
-          withLocalStorage: true,
-          geolocationEnabled: true,
-          withJavascript: true,
-          appCacheEnabled: true,
-          allowFileURLs: true,
-          ignoreSSLErrors: true,
-          initialChild: Container(
-            color: Colors.white,
-            child: const Center(
-              child: CircularProgressIndicator(),
-            ),
+    WebViewController? _controller;
+    final Completer<WebViewController> _controllerCompleter =
+        Completer<WebViewController>();
+    willPopScope() async {
+      Future<bool>? goBack = _controller?.canGoBack();
+      // ignore: unrelated_type_equality_checks
+      if (goBack == true) {
+        return true;
+      } else {
+        _controller!.goBack();
+        return false;
+      }
+    }
+
+    String url = 'https://www.spyxpo.com';
+    return SafeArea(
+      child: WillPopScope(
+        onWillPop: () => willPopScope(),
+        child: Scaffold(
+          body: Builder(
+            builder: (BuildContext context) {
+              return WebView(
+                initialUrl: url,
+                javascriptMode: JavascriptMode.unrestricted,
+                onWebResourceError: (WebResourceError error) {
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => const ErrorPage()),
+                    (Route<dynamic> route) => false,
+                  );
+                },
+                onProgress: (int progress) {
+                  const Center(child: CircularProgressIndicator());
+                },
+                onWebViewCreated: (WebViewController webViewController) {
+                  _controllerCompleter.future
+                      .then((value) => _controller = value);
+                  _controllerCompleter.complete(webViewController);
+                },
+                navigationDelegate: (NavigationRequest request) {
+                  if (request.url.startsWith(url)) {
+                    return NavigationDecision.navigate;
+                  } else {
+                    launchURL(request.url);
+                    return NavigationDecision.prevent;
+                  }
+                },
+                zoomEnabled: false,
+                gestureNavigationEnabled: true,
+                geolocationEnabled: true,
+              );
+            },
           ),
         ),
       ),
     );
+  }
+
+  launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
