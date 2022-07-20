5f49e76244582758cd77a735811933380a20f021
Mantresh Khurana <mantreshkhurana@gmail.com>
2022-07-20 13:40:08 +0530

    Update

--- run.py
+++ run.py
@@ -322,13 +322,8 @@ def saveData():
         f"Your apk is located in \"/build/{app_name_info}/{app_name_info}_{app_version_info}.apk\"\n")
     print(
         f"Your appBundle is located in \"/build/{app_name_info}/{app_name_info}_{app_version_info}.aab\"\n")
-    
-    openBuildfolder()
 
-def building_apk_aab_show():
-   building_apk_aab.pack()
-def building_apk_aab_hide():
-   building_apk_aab.pack_forget()
+    openBuildfolder()
 
 # version details
 version_info = open('version.info', 'r')
